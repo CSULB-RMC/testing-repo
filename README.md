f39720f9b23159a5794770b804f62f7b9da56ce6
Fenteale <mangmasta@gmail.com>
2021-09-14 19:53:00 -0700

    Added a super basic python program
    Also added just a bit more onto the README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testing-repo
 Go hog wild in here.
+
+Feel free to edit this README or anything else to see what it does!
---
+++ main.py
@@ -0,0 +1,14 @@
+# Name: HELLO WORLD
+# Author: Fenteale
+# 
+# Notes:
+# This is just a tiny program meant to showcase what
+# a repository will look like when downloaded through
+# github.
+
+print("Hello World!")
+print("This is just meant to be a tiny program to show off what a repo looks like.")
+
+inp = input("Type something here: ")
+
+print("You typed:", inp)
